55a270763ba3e7aa3008f98d9af6e13c627beab3
EmilisSaltys <47056599+EmilisSaltys@users.noreply.github.com>
2019-03-15 20:49:43 +0200

    game

---
+++ Runner2DBlack/Assets/Scripts/Controler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controler : MonoBehaviour
+{
+
+    private Vector2 targetPos;
+    public float MaxHight;
+    public float MinHight;
+    public int health = 3;
+   
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+
+        if (health == 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 0);
+        }
+        
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < MaxHight)
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y + 3);
+            transform.position = targetPos;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > MinHight )
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y - 3);
+            transform.position = targetPos;
+        }
+
+    }
+}
---
+++ Runner2DBlack/Assets/Scripts/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject obstacle;
+
+    public float Timebtwspawn;
+    public float Starttimebtwspawn;
+
+    private void Update()
+    {
+        if (Timebtwspawn <= 0)
+        {
+            Instantiate(obstacle, transform.position, Quaternion.identity);
+            Timebtwspawn = Starttimebtwspawn;
+
+        }
+        else
+        {
+            Timebtwspawn -= Time.deltaTime;
+        }
+    }
+
+}
